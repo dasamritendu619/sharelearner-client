0d9444d7c4f3fd401141198d0750783ead36439e
Shuvadip Dian <shuvadipdian112@gmail.com>
2025-04-02 01:04:49 +0530

    getOtherUsers bug fixed

--- src/hooks/useGetOtherUsers.jsx
+++ src/hooks/useGetOtherUsers.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setOtherUsers } from '../store/userSlice';
 import { authService } from '@/apiServices/authServices';
@@ -10,13 +9,9 @@ const useGetOtherUsers = () => {
   useEffect(() => {
     const fetchOtherUsers = async () => {
       try {
-        // axios.defaults.withCredentials = true;
-        // const res = await axios.get(`http://localhost:8080/api/v1/user`);
         const responce = await authService.getOtherUsers();
-        console.log("Other user");
-        console.log(responce);  // user not found
-        // // store
-        // dispatch(setOtherUsers(responce.data.data))
+        // store
+        dispatch(setOtherUsers(responce.data))
       } catch (error) {
         console.log(error)
       }
