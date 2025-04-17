5752ec6dd042473b105026f09fcdfa473f67666e
Shuvadip Dian <shuvadipdian112@gmail.com>
2025-04-17 13:28:40 +0530

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,47 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({ 
+      workbox: {
+        maximumFileSizeToCacheInBytes: 6291456, // 6 MB
+      },
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Share Learner(Made by Hackstreet Boys)',
+        short_name: 'Share Learner',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: 'pwa-64x64.png',
+                sizes: '64x64',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
+        ],
+      }, 
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
