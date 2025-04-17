87477b98ebd803c12d62cacd4cc8b915708b521b
Shuvadip Dian <shuvadipdian112@gmail.com>
2025-04-17 15:02:42 +0530

    Add PWA

--- vite.config.js
+++ vite.config.js
@@ -15,25 +15,28 @@ export default defineConfig({
         name: 'Share Learner(Made by Hackstreet Boys)',
         short_name: 'Share Learner',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        description: 'Discover our innovative social media platform, where sharing your learnings and experiences takes center stage.',
         icons: [
             {
-                src: 'pwa-64x64.png',
+                src: '/pwa-64x64.png',
                 sizes: '64x64',
                 type: 'image/png'
             },
             {
-                src: 'pwa-192x192.png',
+                src: '/pwa-192x192.png',
                 sizes: '192x192',
                 type: 'image/png'
             },
             {
-                src: 'pwa-512x512.png',
+                src: '/pwa-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
                 purpose: 'any'
             },
             {
-                src: 'maskable-icon-512x512.png',
+                src: '/maskable-icon-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
                 purpose: 'maskable'
